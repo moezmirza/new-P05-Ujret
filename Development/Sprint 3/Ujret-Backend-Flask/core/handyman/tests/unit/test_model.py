c15fe8ff1b3030faa3790410d25b908cf04ebc87
Abdul Moez <24100244@lums.edu.pk>
2024-05-03 11:35:49 +0500

    flask re

--- Development/Sprint 3/Ujret-Backend-Flask/core/handyman/tests/unit/test_model.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from core.handyman.domain import model as mdl
-
-def test_task_create(seed_task):
-    task: mdl.Task = seed_task()
-
-    assert task.status == mdl.TaskStatus.PENDING
-
